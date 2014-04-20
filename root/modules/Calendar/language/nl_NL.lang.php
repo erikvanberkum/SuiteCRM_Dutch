3e3e5eded38daddd827d4d232b828b3f33fbcb02
erikvanberkum <erik@vanberkum.me>
2014-04-20 22:31:21 +0800

    Changes 20140402
    Calendar and Calls

--- root/modules/Calendar/language/nl_NL.lang.php
+++ root/modules/Calendar/language/nl_NL.lang.php
@@ -1,18 +1,43 @@
 <?php
 if (!defined('sugarEntry') || !sugarEntry)
   die('Not A Valid Entry Point');
-
-/**
+/*********************************************************************************
+ * 
+ * Copyright (c) 2014 Acniti (http:/www.acniti.com) 
  * Copyright (c) 2010-2012 Prudento (http://www.prudento.com)
  * Copyright (c) 2007-2009 Madcap BV (http://www.madcap.nl)
  * All rights reserved.
  *
  * Permission is granted for use, copying, modification, distribution,
  * and distribution of modified versions of this work as long as the
- * above copyright notice is included.
- */
-
-$mod_strings = array (
+ * above copyright notice is included. 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ *  
+ ********************************************************************************/
+ 
+ 
+ $mod_strings = array (
+ 
   'LBL_MODULE_NAME' => 'Agenda',
   'LBL_MODULE_TITLE' => 'Agenda',
   'LNK_NEW_CALL' => 'Nieuw telefoongesprek',
@@ -55,12 +80,103 @@ $mod_strings = array (
   'LBL_FILTER_BY_TEAM' => 'Filter gebruikerslijst per team:',
   'LBL_ASSIGNED_TO_NAME' => 'Toegewezen aan',
   'LBL_DATE' => 'Startdatum & tijd',
-//610
-'LBL_MODULE_ACTION' => 'Bekijk',
+  'LBL_CREATE_MEETING' => 'Plan afsprak',
+  'LBL_CREATE_CALL' => 'Log telefoongesprek',
+  'LBL_HOURS_ABBREV' => 'u',
+  'LBL_MINS_ABBREV' => 'm',
+
+
+  'LBL_YES' => 'Ja',
+  'LBL_NO' => 'Nee',
+  'LBL_SETTINGS' => 'Instellingen',
+  'LBL_CREATE_NEW_RECORD' => 'Creeer Activiteit',
+  'LBL_LOADING' => 'Laden...',
+  'LBL_SAVING' => 'Opslaan...',
+  'LBL_SENDING_INVITES' => 'Opslaan & Versturen Genodigden .....',
+  'LBL_CONFIRM_REMOVE' => 'Weet u zeker dat u dit record wilt verwijderen?',
+  'LBL_CONFIRM_REMOVE_ALL_RECURRING' => 'Weet u zker dat u al deze records wilt verwijderen?',
+  'LBL_EDIT_RECORD' => 'Bewerkt activiteit',
+  'LBL_ERROR_SAVING' => 'Fout bij opslaan',
+  'LBL_ERROR_LOADING' => 'Fout bij laden',
+  'LBL_GOTO_DATE' => 'Ga naar datum',
+  'NOTICE_DURATION_TIME' => 'Tijdsduur moet groter zijn dan 0',
+  'LBL_STYLE_BASIC' => 'Basis',
+  'LBL_STYLE_ADVANCED' => 'Geavanceerd',
+  
+  'LBL_INFO_TITLE' => 'Extra details',
+  'LBL_INFO_DESC' => 'Omschrijving',
+  'LBL_INFO_START_DT' => 'Start datum tijd',
+  'LBL_INFO_DUE_DT' => 'Vervaldag tijd',
+  'LBL_INFO_DURATION' => 'Duur',
+  'LBL_INFO_NAME' => 'Onderwerp',
+  'LBL_INFO_RELATED_TO' => 'Gerelateerd aan',
+  
+  'LBL_NO_USER' => 'Geen match voor veld: Toegekend aan',
+  'LBL_SUBJECT' => 'Onderwerp',
+  'LBL_DURATION' => 'Duur',
+  'LBL_STATUS' => 'Status',
+  'LBL_DATE_TIME' => 'Datum en tijd',
+  
+
+  'LBL_SETTINGS_TITLE' => 'Instellingen',
+  'LBL_SETTINGS_DISPLAY_TIMESLOTS' => 'Toon tijden in Dag en Week stijl:',
+  'LBL_SETTINGS_TIME_STARTS' => 'Start tijd:',
+  'LBL_SETTINGS_TIME_ENDS' => 'Eind tijd:',
+  'LBL_SETTINGS_CALLS_SHOW' => 'Toon taken:',
+  'LBL_SETTINGS_CALLS_SHOW' => 'Toon telefoongesprekken:',
+  'LBL_SETTINGS_TASKS_SHOW' => 'Toon taken:',
+  
+  'LBL_SAVE_BUTTON' => 'Opslaan',
+  'LBL_DELETE_BUTTON' => 'Verwijderen',
+  'LBL_APPLY_BUTTON' => 'Toepassen',
+  'LBL_SEND_INVITES' => 'Verstuur uitnodigingen',
+  'LBL_CANCEL_BUTTON' => 'Annuleer',
+  'LBL_CLOSE_BUTTON' => 'Sluiten',
+  
+  'LBL_GENERAL_TAB' => 'Details',
+  'LBL_PARTICIPANTS_TAB' => 'Uitgenodigden',
+  'LBL_REPEAT_TAB' => 'Herhaling',	
+	
+	'LBL_REPEAT_TYPE' => 'Herhaal',
+	'LBL_REPEAT_INTERVAL' => 'Allen',
+	'LBL_REPEAT_END' => 'Eind',	
+	'LBL_REPEAT_END_AFTER' => 'Na',
+	'LBL_REPEAT_OCCURRENCES' => 'Herhalingen',
+	'LBL_REPEAT_END_BY' => 'Door',	
+	'LBL_REPEAT_DOW' => 'Op/Verder',	
+	'LBL_REPEAT_UNTIL' => 'Herhaal tot',
+	'LBL_REPEAT_COUNT' => 'Aantal Herhalingen',
+	'LBL_REPEAT_LIMIT_ERROR' => 'Uw verzoek gaat meer dan de toegestane $limit van afspraken creeeren.',
+	
+	'LBL_EDIT_ALL_RECURRENCES' => 'Bewerk alles',
+	'LBL_REMOVE_ALL_RECURRENCES' => 'Verwijder alles',
+
+	'LBL_DATE_END_ERROR' => 'Eind datum is voor de start datum',
+	'ERR_YEAR_BETWEEN' => 'Sorry, de agenda kan niet in dit jaar plannen<br>Het jaar moet tussen 1970 en 2037 zijn',
+	'ERR_NEIGHBOR_DATE' => 'get_neighbor_date_str: niet gedefineerd voor deze opmaak',
+
 );
 
 $mod_list_strings = array (
-  'dom_cal_month' => array (
+    'dom_cal_weekdays' => array (
+    '0' => 'Zo',
+    '1' => 'Ma',
+    '2' => 'Di',
+    '3' => 'Wo',
+    '4' => 'Do',
+    '5' => 'Vr',
+    '6' => 'Za'
+  ),
+  'dom_cal_weekdays_long' => array (
+    '0' => 'Zondag',
+    '1' => 'Maandag',
+    '2' => 'Dinsdag',
+    '3' => 'Woensdag',
+    '4' => 'Donderdag',
+    '5' => 'Vrijdag',
+    '6' => 'Zaterdag'
+  ),
+   'dom_cal_month' => array (
     '0' => '',
     '1' => 'Jan',
     '2' => 'Feb',
@@ -90,69 +206,4 @@ $mod_list_strings = array (
     '11' => 'November',
     '12' => 'December'
   ),
-  'dom_cal_weekdays' => array (
-    '0' => 'Zo',
-    '1' => 'Ma',
-    '2' => 'Di',
-    '3' => 'Wo',
-    '4' => 'Do',
-    '5' => 'Vr',
-    '6' => 'Za'
-  ),
-  'dom_cal_weekdays_long' => array (
-    '0' => 'Zondag',
-    '1' => 'Maandag',
-    '2' => 'Dinsdag',
-    '3' => 'Woensdag',
-    '4' => 'Donderdag',
-    '5' => 'Vrijdag',
-    '6' => 'Zaterdag'
-  ),
-
-
-'LBL_EDIT_USERLIST' => 'Gebruikers lijst',
-'LBL_CREATE_MEETING' => 'Plan afsprak',
-'LBL_CREATE_CALL' => 'Log telefoongesprek',
-'LBL_HOURS_ABBREV' => 'u',
-'LBL_MINS_ABBREV' => 'm',
-'LBL_YES' => 'Ja',
-'LBL_NO' => 'Nee',
-'LBL_SETTINGS' => 'Instellingen',
-'LBL_LOADING' => 'Laden...',
-'LBL_SAVING' => 'Opslaan...',
-'LBL_CONFIRM_REMOVE' => 'Weet u zeker dat u dit record wilt verwijderen?',
-'LBL_EDIT_RECORD' => 'Bewerkt activiteit',
-'LBL_ERROR_SAVING' => 'Fout bij opslaan',
-'LBL_ERROR_LOADING' => 'Fout bij laden',
-'LBL_GOTO_DATE' => 'Ga naar datum',
-'NOTICE_DURATION_TIME' => 'Tijdsduur moet groter zijn dan 0',
-'LBL_STYLE_BASIC' => 'Basis',
-'LBL_STYLE_ADVANCED' => 'Geavanceerd',
-'LBL_INFO_TITLE' => 'Extra details',
-'LBL_INFO_DESC' => 'Omschrijving',
-'LBL_INFO_START_DT' => 'Start datum tijd',
-'LBL_INFO_DUE_DT' => 'Vervaldag tijd',
-'LBL_INFO_DURATION' => 'Duur',
-'LBL_INFO_NAME' => 'Onderwerp',
-'LBL_INFO_RELATED_TO' => 'Gerelateerd aan',
-'LBL_NO_USER' => 'Geen match voor veld: Toegekend aan',
-'LBL_SUBJECT' => 'Onderwerp',
-'LBL_DURATION' => 'Duur',
-'LBL_STATUS' => 'Status',
-'LBL_DATE_TIME' => 'Datum en tijd',
-'LBL_SETTINGS_TITLE' => 'Instellingen',
-'LBL_SETTINGS_TIME_STARTS' => 'Start tijd:',
-'LBL_SETTINGS_TIME_ENDS' => 'Eind tijd:',
-'LBL_SETTINGS_CALENDAR_STYLE' => 'Agenda stijl:',
-'LBL_SETTINGS_CALLS_SHOW' => 'Toon taken:',
-'LBL_SETTINGS_CALLS_SHOW' => 'Toon telefoongesprekken:',
-'LBL_SETTINGS_TASKS_SHOW' => 'Toon taken:',
-'LBL_SAVE_BUTTON' => 'Opslaan',
-'LBL_DELETE_BUTTON' => 'Verwijderen',
-'LBL_APPLY_BUTTON' => 'Toepassen',
-'LBL_SEND_INVITES' => 'Verstuur uitnodigingen',
-'LBL_CANCEL_BUTTON' => 'Annuleer',
-'LBL_CLOSE_BUTTON' => 'Sluiten',
-'LBL_GENERAL_TAB' => 'Details',
-'LBL_PARTICIPANTS_TAB' => 'Uitgenodigden',
 );
--- root/modules/Calls/language/nl_NL.lang.php
+++ root/modules/Calls/language/nl_NL.lang.php
@@ -1,16 +1,39 @@
 <?php
 if (!defined('sugarEntry') || !sugarEntry)
   die('Not A Valid Entry Point');
-
-/**
+/*********************************************************************************
+ * 
+ * Copyright (c) 2014 Acniti (http:/www.acniti.com) 
  * Copyright (c) 2010-2012 Prudento (http://www.prudento.com)
  * Copyright (c) 2007-2009 Madcap BV (http://www.madcap.nl)
  * All rights reserved.
  *
  * Permission is granted for use, copying, modification, distribution,
  * and distribution of modified versions of this work as long as the
- * above copyright notice is included.
- */
+ * above copyright notice is included. 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ *
+ *  
+ ********************************************************************************/
 
 $mod_strings = array (
   "LBL_BLANK" => "  ",
